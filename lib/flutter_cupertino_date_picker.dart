c82237b129c20cc5f5570c572d5a9890f8d96880
zyme <zymer@foxmail.com>
2020-11-30 23:10:02 +0800

    bugfix

--- lib/flutter_cupertino_date_picker.dart
+++ lib/flutter_cupertino_date_picker.dart
@@ -6,3 +6,4 @@ export 'src/date_picker_theme.dart' show DateTimePickerTheme;
 export 'src/widget/date_picker_widget.dart';
 export 'src/widget/time_picker_widget.dart';
 export 'src/widget/datetime_picker_widget.dart';
+export 'src/widget/date_picker_bottom_widget.dart';
